2784ac58b5cf0942ed8c0a193f37c05f457587fa
noahsutsmo@gmail.com <noahsutsmo@gmail.com@48954bed-5b59-45a6-e39c-c6a33cf3ab01>
2014-01-29 06:27:34 +0000

    Moved waitForStart out of JoystickDriver4278.c - it doesn't belong there. Removed a few dead lines from JoystickDriver4278.c.

--- trunk/drivers/JoystickDriver4278.c
+++ trunk/drivers/JoystickDriver4278.c
@@ -1,5 +1,5 @@
-#ifndef __JOSTICKDRIVERC4278__
-#define __JOSTICKDRIVERC4278__
+#ifndef __JDRIVER4278__
+#define __JDRIVER4278__
 
 #pragma autoStartTasks
 #pragma systemFile
@@ -30,12 +30,11 @@ bool joy1Btn(int btn) {return ((joystick.joy1_Buttons & (1 << btn)) != 0);}
 const TMailboxIDs kJoystickQueueID = mailbox1;
 TJoystick joystickCopy;  // Internal buffer to hold the last received message from the PC. Do not use
 
-long ntotalMessageCount = 0;
-
 bool bDisconnected = false;
 bool bOverrideJoystickDisabling = false;
 long nNoMessageCounterLimit = 500;
 long nNoMessageCounter = 0;
+
 task readMsgFromPC() {
   bool bMsgFound;
 
@@ -62,11 +61,11 @@ task readMsgFromPC() {
       int nSizeOfMessage;
       nSizeOfMessage = cCmdMessageGetSize(kJoystickQueueID);
 
-      if (nSizeOfMessage <= 0) {
-        if (!bMsgFound) {
-          if (nNoMessageCounter > nNoMessageCounterLimit) {
+      if(nSizeOfMessage <= 0) {
+        if(!bMsgFound) {
+          if(nNoMessageCounter > nNoMessageCounterLimit) {
             hogCPU();
-            if (!bOverrideJoystickDisabling) {
+            if(!bOverrideJoystickDisabling) {
               bTempUserMode = joystickCopy.UserMode;
               bTempStopPgm = joystickCopy.StopPgm;
 
@@ -87,49 +86,30 @@ task readMsgFromPC() {
           nNoMessageCounter = 0;
           break;
         }
-        // No more messages available and at least one message found. This is not essential but
-        // useful to ensure that we're working with the latest message. We simply discard earlier
-        // messages. This is useful because there could be many messages queued and we don't
-        // want to work with stale data.
       }
 
       if(nSizeOfMessage > sizeof(tempBuffer)) {nSizeOfMessage = sizeof(tempBuffer);}
       nBTCmdRdErrorStatus = cCmdMessageRead(&tempBuffer[0], nSizeOfMessage, kJoystickQueueID);
       nBTCmdRdErrorStatus = nBTCmdRdErrorStatus; //Get rid of info message
 
-      // Repeat loop until there are no more messages in the queue. We only want to process the
-      // last message in the queue.
       bMsgFound = true;
     }
 
-    hogCPU();   // grab CPU for duration of critical section
-
-    ++ntotalMessageCount;
-
+    hogCPU();
     joystickCopy.UserMode           = (bool)tempBuffer[1];
     joystickCopy.StopPgm            = (bool)tempBuffer[2];
 
     joystickCopy.joy1_x1            = tempBuffer[3];
-    joystickCopy.joy1_y1            = tempBuffer[4];// Negate to "natural" position
+    joystickCopy.joy1_y1            = tempBuffer[4];
     joystickCopy.joy1_x2            = tempBuffer[5];
-    joystickCopy.joy1_y2            = tempBuffer[6];// Negate to "natural" position
+    joystickCopy.joy1_y2            = tempBuffer[6];
     joystickCopy.joy1_Buttons       = (tempBuffer[7] & 0x00FF) | (tempBuffer[8] << 8);
     joystickCopy.joy1_TopHat        = tempBuffer[9];
 
-    joystickCopy.joy1_y1            = -joystickCopy.joy1_y1; // Negate to "natural" position
-    joystickCopy.joy1_y2            = -joystickCopy.joy1_y2; // Negate to "natural" position
-
-    releaseCPU(); // end of critical section
+    joystickCopy.joy1_y1            = -joystickCopy.joy1_y1;
+    joystickCopy.joy1_y2            = -joystickCopy.joy1_y2;
+    releaseCPU();
   }
 }
 
-void waitForStart() {
-  while(true) {
-    getJoystickSettings(joystick);
-    if(!joystick.StopPgm) break;
-		//nxtDisplayTextLine(6, "Waiting for start...");
-    //displayDiagnostics();
-  }
-}
-
-#endif
+#endif // __JDRIVER4278__
